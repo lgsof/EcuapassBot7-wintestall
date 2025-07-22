8a48cc4523aeb71b9307ee14321b982c4bde7bc3
Luis Garreta <luisgarreta@yahoo.com>
2025-07-22 10:21:34 -0500

    Jul/21: r0.9875: Codebini Install. Fixed Settings onTop: Frame instead Dialog. Splash window.

--- splash.ps1
+++ splash.ps1
@@ -21,7 +21,7 @@ $window.Content = $label
 # Start background job to launch your app and close splash after it exits
 Start-Job {
     Start-Sleep -Milliseconds 500
-    Start-Process -FilePath ".\EcuapassBot.bat" -Wait
+    Start-Process -FilePath "C:\Users\LuisG\AppData\Local\Programs\EcuapassBot7-wintest\EcuapassBot.bat" -Wait
     [System.Windows.Application]::Current.Dispatcher.Invoke({ $window.Close() })
 } | Out-Null
 
--- splash_ps.vbs
+++ splash_ps.vbs
@@ -1,5 +1,5 @@
 Set shell = CreateObject("WScript.Shell")
-shell.CurrentDirectory = "C:\Users\LuisG\AppData\Local\Programs\EcuapassBot7-wintest"
+'shell.CurrentDirectory = "C:\Users\LuisG\AppData\Local\Programs\EcuapassBot7-wintest"
 
 splashScript = """C:\Users\LuisG\AppData\Local\Programs\EcuapassBot7-wintest\splash.ps1"""
 cmd = "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File " & splashScript
